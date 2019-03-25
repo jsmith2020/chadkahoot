e4ea4ef44171f112b36cd1375b271aa05ddba5f9
mpresson333 <47357283+mpresson333@users.noreply.github.com>
2019-03-25 14:15:27 -0400

    image function

--- chadkahoot.py
+++ chadkahoot.py
@@ -15,6 +15,68 @@ YELLOW = (255, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+def load_images():
+    global kahoot_image
+    karen_image = pygame.image.load('kid-suitcase.jpg')
+    global ligma_image
+    ligma_image = pygame.image.load('ligma.jpg')
+    global lizard_image
+    lizard_image = pygame.image.load('lizard.jpg')
+    global knuckles_image
+    knuckles_image = pygame.image.load('knuckles.jpg')
+    global harambe_image
+    harambe_image = pygame.image.load('harambe.jpg')
+    global spaghet_image
+    spaghet_image = pygame.image.load('spaghet.jpg')
+    global ali_a_image
+    ali_a_image = pygame.image.load('ali-a.png')
+    global johnny_image
+    johnny_image = pygame.image.load('johnny.jiff')
+    global fortnite_image
+    fortnite_image = pygame.image.load('fortnite.png')
+    global pokemon_image
+    pokemon_image = pygame.image.load('pokemon.jpg')
+    global dislike_image
+    dislike_image = pygame.image.load('dislike.jiff')
+    global mario_image
+    mario_image = pygame.image.load('mario.jpg')
+    global PS4_image
+    PS4_image = pygame.image.load('PS4.jpeg')
+    global gtav_image
+    gtav_image = pygame.image.load('gtav.jpg')
+    global fifa_image
+    fifa_image = pygame.image.load('fifa-14.jpg')
+    global csgo_image
+    csgo_image = pygame.image.load('csgo.jpg')
+    global hitler_image
+    hitler_image = pygame.image.load('gitler.jpg')
+    global mussolini_image
+    mussolini_image = pygame.image.load('mussolini.jpg')
+    global WWII_image
+    WWII_image = pygame.image.load('WWII.jpg')
+    global battle_of_the_bulge_image
+    battle_of_the_bulge_image = pygame.image.load('battle_of_the_bulge.jpg')
+    global invasion_of_china_image
+    invasion_of_china_image = pygame.image.load('invasion_of_china.jpg')
+    global battle_of_atlantic_image
+    battle_of_atlantic_image = pygame.image.load('atlantic-battle.jpg')
+    global dachau_image
+    dachau_image = pygame.image.load('dachau.jpeg')
+    global world_series_image
+    world_series_image = pygame.image.load('world_series.png')
+    global horse_image
+    horse_image = pygame.image.load('horse.jpg')
+    global admiral_image
+    admiral_image = pygame.image.load('admial.png')
+    global NBA_2K_image
+    NBA_2K_image = pygame.image.load('2K.jpg')
+    global tour_de_france_image
+    tour_de_france_image = pygame.image.load('tour_de_france.jpg')
+    global jai_alai_image
+    jai_alai_image = pygame.image.load('jai_alai.jpg')
+    global world_cup_image
+    world_cup_image = pygame.image.load('world_cup.jpg')
+
 def ask_question(color):
     frametime = 0
     if frametime <= 1000:
